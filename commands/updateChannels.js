e9b837b36298ddd133a7e38a7c4a0a009050471e
Thomas Priftakis <tpriftakis@gmail.com>
2023-01-10 15:45:57 -0500

    added choice classes for the dataset

--- commands/updateChannels.js
+++ commands/updateChannels.js
@@ -28,24 +28,33 @@ module.exports = {
             const filePath = path.join(disciplineDataPath, file);
             const discipline = require(filePath);
 
-            function updateClassChannel(course){
+            function updateClassChannel(course, topic){
                 course = course.toLowerCase().replace(' ', '-');
                 if(!(channels.includes(course))){
                     guild.channels.create({
                         name: course,
                         type: ChannelType.GuildText,
+                        topic: topic,
                     });
                 }
             }
 
-            for(const course of Object.keys(discipline["common core courses"])){
-                updateClassChannel(course);
+            for(const course in discipline["common core courses"]){
+                const topic = discipline["common core courses"][course];
+                updateClassChannel(course, topic);
             }
             
-            for(const subPlan of Object.keys(discipline["sub-plans"])){
+            for(const subPlan in discipline["sub-plans"]){
                 const courses = Object.keys(discipline["sub-plans"][subPlan]["courses"]);
                 for(const course of courses){
-                    updateClassChannel(course);
+                    const topic = discipline["sub-plans"][subPlan]["courses"][course];
+                    updateClassChannel(course, topic);
+                }
+            }
+            for(const choice of discipline["choices"]){
+                for(const course in choice){
+                    const topic = discipline["choices"][course];
+                    updateClassChannel(course, topic);
                 }
             }
 
