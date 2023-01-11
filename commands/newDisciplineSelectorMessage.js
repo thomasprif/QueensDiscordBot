d6af6a979aa057d809214e8aa0962f90e6f8615d
Thomas Priftakis <tpriftakis@gmail.com>
2023-01-10 19:05:05 -0500

    Fixed width error bug

--- commands/newDisciplineSelectorMessage.js
+++ commands/newDisciplineSelectorMessage.js
@@ -13,24 +13,43 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
         const disciplines = getDisciplines().map(discipline => discipline["name"]);
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId("Not Sure")
-                    .setLabel("Not Sure")
-                    .setStyle(ButtonStyle.Primary),
-                ...disciplines.map(name => {
-                    return new ButtonBuilder()
-                        .setCustomId(name)
-                        .setLabel(name)
-                        .setStyle(ButtonStyle.Primary);
-                }),
-            );
+        const rows = [];
+
+        for(let i = 0; i < disciplines.length; i += 5){
+            if(i + 5 >= disciplines.length){
+                rows.push(disciplines.slice(i, disciplines.length));
+                break;
+            }
+            rows.push(disciplines.slice(i, i + 5));
+        }
+
+        rows.forEach((value, index) => {
+            rows[index] = new ActionRowBuilder()
+                .addComponents(
+                    ...value.map(name => {
+                        return new ButtonBuilder()
+                            .setCustomId(name)
+                            .setLabel(name)
+                            .setStyle(ButtonStyle.Primary);
+                    }),
+                );
+        });
+
+        rows.push(
+        new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId("Not Sure")
+                .setLabel("Not Sure")
+                .setStyle(ButtonStyle.Primary),
+        ));
+
+
 
         const embed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle('Choose Your Discipline!');
 
-         interaction.reply({ content: "", embeds: [embed], components: [row] });
+         interaction.reply({ content: "", embeds: [embed], components: rows });
     },
 };
